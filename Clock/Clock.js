d3da7406969a0f9eab1c100a3eca21d56c5ef004
Siddeshgowda <121502684+Siddeshgowda20@users.noreply.github.com>
2023-03-15 20:54:20 +0530

    Add files via upload

---
+++ Clock/Clock.js
@@ -0,0 +1,49 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const years= ["2020","2021","2022","2023","2024","2025","2026","2027","2028","2029","2030"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode';
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode';
+    }
+})
+
+function setTime() {
+    const time = new Date();
+    const year = time.getFullYear()
+    const month = time.getMonth()
+    const day = time.getDay()
+    const date = time.getDate()
+    const hours = time.getHours()
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursForClock<10 ? `0${hoursForClock}`:hoursForClock}:${minutes < 10 ?`0${minutes}`:minutes}:${seconds < 10 ?`0${seconds}`:seconds} ${ampm}`
+    dateEl.innerHTML = `  <span class="circle">${date}</span> - ${days[day]} - ${months[month]} - ${years[year]}`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
